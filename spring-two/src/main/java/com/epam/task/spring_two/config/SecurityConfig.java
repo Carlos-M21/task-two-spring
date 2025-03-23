e97c6783dae02001833ba049cefe0a2e8cc0f86a
Carlos-M21 <cristianmathers1107@gmail.com>
2025-03-23 18:20:47 -0500

    add jwt and auth validations

---
+++ spring-two/src/main/java/com/epam/task/spring_two/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.epam.task.spring_two.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/api/books").permitAll()
+                        .requestMatchers("/api/books/**").authenticated()
+                )
+                .cors(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(withDefaults())
+                )
+                .build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        String jwkSetUri = "https://dev-w1csf1wc8blmmzi2.us.auth0.com/.well-known/jwks.json"; // Replace with your Auth0 domain
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+}
--- spring-two/src/main/java/com/epam/task/spring_two/controller/BookController.java
+++ spring-two/src/main/java/com/epam/task/spring_two/controller/BookController.java
@@ -4,38 +4,43 @@ import com.epam.task.spring_two.model.Book;
 import com.epam.task.spring_two.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/books")
 @RequiredArgsConstructor
 public class BookController {
     private final BookService bookService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('SCOPE_readbook')")
     public ResponseEntity<List<Book>> getAllBooks() {
         return ResponseEntity.ok(bookService.getAllBooks());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_readbook')")
     public ResponseEntity<Book> getBookById(@PathVariable Long id) {
         return ResponseEntity.ok(bookService.getBookById(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('SCOPE_updatebook')")
     public ResponseEntity<Book> createBook(@RequestBody Book book) {
         return ResponseEntity.ok(bookService.createBook(book));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_updatebook')")
     public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
         return ResponseEntity.ok(bookService.updateBook(id, book));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_updatebook')")
     public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
         bookService.deleteBook(id);
         return ResponseEntity.noContent().build();
